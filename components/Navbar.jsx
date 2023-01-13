b5bfe03013718afda67a4ce37620823b741388e0
mezzat01 <mezzatamine2002@gmail.com>
2023-01-13 17:47:35 +0100

    first push of the project

---
+++ components/Navbar.jsx
@@ -0,0 +1,25 @@
+'use client';
+
+//  importing motion that uses client hooks
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`} />
+    <img
+      src="/search.svg"
+    />
+  </motion.nav>
+);
+
+export default Navbar;
