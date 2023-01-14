b1f5e85b54bc06ecc79c059aaa675d94e3afe433
Mohamed Amine Mezzat <mmezzat@e1r8p3.1337.ma>
2023-01-14 15:11:25 +0100

    completed the navbar of the website

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,10 +15,21 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`} />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`} >
     <img
       src="/search.svg"
+      alt="searching logo"
+      className="w-[24px] h-[24px] object-contain"
     />
+    <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+    THE NEW WORLDS
+    </h2>
+    <img 
+      src="/menu.svg" 
+      alt="menu" 
+      className="w-[24px] h-[24px] object-contain"
+    />
+    </div>
   </motion.nav>
 );
 
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
 const Hero = () => (
   <section>
     Hero section
