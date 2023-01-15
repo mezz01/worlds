f8d9e87d131dd32459aa4a0c113be168fb65caf3
Mohamed Amine Mezzat <mmezzat@e1r7p5.1337.ma>
2023-01-15 17:38:19 +0100

    creating the about section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => (
       viewport={{ once: false, amount: 0.25}}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <div className="flex justify-center items-center flex-col relative z-10">
+      <div className="flex justify-center items-center flex-col relative z-10" >
 
         <motion.h1 variants={textVariant(1.1)}
         className={styles.heroHeading}>
@@ -33,6 +33,18 @@ const Hero = () => (
           <h2 className={styles.heroHeading}>s</h2>
         </motion.div>
       </div>
+      <motion.div 
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top[30px]" />
+        <img src="/cover.png" alt="cover" className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative" />
+      <a href="#explore">
+        <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+          <img src="/stamp.png" alt="stamp" className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain" />
+        </div>
+      </a>
+      </motion.div>
     </motion.div>
   </section>
 );
