bb597f698f7fef702779ccfac176663a6ff815b7
Mohamed Amine Mezzat <mezzatamine2002@gmail.com>
2023-01-15 23:27:50 +0100

    end of the day changes

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-
 import styles from '../styles';
 import { TypingText } from '../components';
 import { fadeIn, staggerContainer } from '../utils/motion';
@@ -11,6 +10,9 @@ const About = () => (
     <div className="gradient-02 z-0"/>
     <motion.div
       variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once:false, amount: 0.25}}
     >
 
     </motion.div>
