319902a161afe1bc029b4b03972f1b746490e020
Mohamed Amine Mezzat <mezzatamine2002@gmail.com>
2023-01-15 19:08:27 +0100

    fixing the style and the github email prblm

--- sections/About.jsx
+++ sections/About.jsx
@@ -7,8 +7,13 @@ import { TypingText } from '../components';
 import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
-  <section>
-    this section 
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0"/>
+    <motion.div
+      variants={staggerContainer}
+    >
+
+    </motion.div>
   </section>
 );
 
