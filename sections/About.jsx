3dfd1cecaf71108c72b70868cb4ed8e961e41559
Mohamed Amine Mezzat <mmezzat@e1r7p5.1337.ma>
2023-01-15 17:50:23 +0100

    fkn work plz

--- sections/About.jsx
+++ sections/About.jsx
@@ -3,7 +3,8 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { TypingText } from '../components';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
   <section>
