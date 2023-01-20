2362257711ac772f35caf24cdaae47e2c376b1f2
Mohamed Amine Mezzat <mezzatamine2002@gmail.com>
2023-01-20 15:48:46 +0100

    creating the index.js for some constants

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,25 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+
+
+import { ExploreCard,TypingText,TitleText } from '../components';
+import { staggerContainer } from '../utils/motion';
+import { exploreWorlds } from '../constants'
+
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: false, amount: 0.25}}
+      className={}
+    >
+
+    </motion.div>
   </section>
 );
 
