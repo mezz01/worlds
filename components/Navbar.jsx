fc7d1078c1f09cd13eb9de363b5f51ef73311872
mezzat01 <mezzatamine2002@gmail.com>
2023-01-22 23:23:54 +0100

    correcting the jsx on two files

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,20 +15,20 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`} >
-    <img
-      src="/search.svg"
-      alt="searching logo"
-      className="w-[24px] h-[24px] object-contain"
-    />
-    <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-    META WORLDS
-    </h2>
-    <img 
-      src="/menu.svg" 
-      alt="menu" 
-      className="w-[24px] h-[24px] object-contain"
-    />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="searching logo"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        META WORLDS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
     </div>
   </motion.nav>
 );
--- sections/About.jsx
+++ sections/About.jsx
@@ -7,25 +7,24 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
-    <div className="gradient-02 z-0"/>
+    <div className="gradient-02 z-0" />
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{once:false, amount: 0.25}}
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title="| About metaworlds" textStyles="text-center" />
       <motion.p
-        variants = {fadeIn('up', 'tween', 0.2, 1)}
-        className = "mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
-        
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-      <span className="font-extrabold text-white">Blockchain </span>
-       is a decentralized digital ledger that records transactions across a network of computers. It is the underlying technology behind cryptocurrencies like Bitcoin and Ethereum, but it has potential uses beyond just digital currencies. Each block in a blockchain contains a list of transactions, and once a block is added to the chain it cannot be altered. This creates a tamper-proof record of all transactions that is maintained by a network of computers, rather than a central authority. The distributed nature of blockchain technology also allows for increased security and transparency. The technology is still in its early stages and its potential use cases are being explored in various industries such as finance, supply chain, and healthcare.
+        <span className="font-extrabold text-white">Blockchain </span>
+        is a decentralized digital ledger that records transactions across a network of computers. It is the underlying technology behind cryptocurrencies like Bitcoin and Ethereum, but it has potential uses beyond just digital currencies. Each block in a blockchain contains a list of transactions, and once a block is added to the chain it cannot be altered. This creates a tamper-proof record of all transactions that is maintained by a network of computers, rather than a central authority. The distributed nature of blockchain technology also allows for increased security and transparency. The technology is still in its early stages and its potential use cases are being explored in various industries such as finance, supply chain, and healthcare.
       </motion.p>
       <motion.img
-        variants={fadeIn('up','tween', 0.3, 1)}
+        variants={fadeIn('up', 'tween', 0.3, 1)}
         src="/arrow-down.svg"
         alt="arrow down"
         className="w-[18px] h-[28px] object-contain mt-[28px]"
