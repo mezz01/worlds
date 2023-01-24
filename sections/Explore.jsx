08d4ac24041d82a41c46007bcb0099b7cd79f59e
mezzat01 <mezzatamine2002@gmail.com>
2023-01-24 23:38:48 +0100

    advencing on the explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -9,18 +9,32 @@ import { ExploreCard,TypingText,TitleText } from '../components';
 import { staggerContainer } from '../utils/motion';
 import { exploreWorlds } from '../constants'
 
-const Explore = () => (
+const Explore = () => {
+  useState
+  return (
   <section className={`${styles.paddings}`} id="explore">
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       viewport={{once: false, amount: 0.25}}
-      className={}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-
+      <TypingText title="|  The World" textStyles="text-center" />
+      <TypingText title={<>Choose the world you want<br className="md:block hidden" /*visible on medium devices but usually hidden*//> to explore </>} textStyles="text-center" />
+      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5" >
+        {exploreWorlds.map((world, index) => (
+          <ExploreCard key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            hand
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
+  )
+};
 
 export default Explore;
